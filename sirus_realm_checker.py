d9f7e41b7445d18a5621c1d5399830dbcb642d3e
datol <dmitry_korj@icloud.com>
2025-02-08 13:51:24 +0500

    fix

--- sirus_realm_checker.py
+++ sirus_realm_checker.py
@@ -1,8 +1,8 @@
 from time import sleep
 import json
 import logging
-import shutil
-import tempfile
+
+import psutil
 import requests
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
@@ -10,24 +10,23 @@ from selenium.webdriver.common.by import By
 TOKEN = "7711822294:AAEN6ywEkaJSV-w2BHB5z8O9dS1sc4AsdX4"
 CHAT_ID = "-1002289513470"
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
 def get_realm_data():
-    driver = uc.Chrome(headless=False, use_subprocess=True, driver_executable_path="/usr/bin/chromedriver")
-    driver.get('https://sirus.su/api/statistic/tooltip.json')
-    response = json.loads(driver.find_element(By.TAG_NAME, 'body').text)
-    driver.close()
     realm_name = "Soulseeker x1 - 3.3.5a+"
+    kill_old_browser()
+    driver = uc.Chrome(headless=False, use_subprocess=True)
     try:
+        driver.get('https://sirus.su/api/statistic/tooltip.jsonz')
+        response = json.loads(driver.find_element(By.TAG_NAME, 'body').text)
         is_online = response['realms'][1]['isOnline']
-    except KeyError:
+    except Exception as e:
+        print(type(e).__name__)
         is_online = False
     finally:
-        temp_folder = tempfile.gettempdir()
-        logger.info(f"Cleaning temp directory: {temp_folder}") 
-        # shutil.rmtree(temp_folder)
+        driver.close()
     return {'isOnline': is_online, 'name': realm_name}
 
 
@@ -44,6 +43,13 @@ def wait_for_server_up():
             send_tg_message(f"{server_name}is up now!")
             break
         sleep(10)
+
+
+def kill_old_browser():
+    for process in psutil.process_iter():
+        if 'firefox' in str(process.name):
+            process.kill()
+            logger.info("Killed old browser")
 
 
 while True:
